75473a30ffdb99c5d55badf34a085a1fd8cc6598
Math Scicl <scicluna.mathieu@hotmail.fr>
2025-11-27 09:11:10 +0100

    modif regle typescript

--- components/RestoCard.tsx
+++ components/RestoCard.tsx
@@ -1,17 +1,29 @@
+import Image from "next/image";
+
+type RestoCardProps = {
+  name: string;
+  logo?: string;
+  description: string;
+  locations: string;
+  link?: string;
+};
+
 export default function RestoCard({
   name,
   logo,
   description,
   locations,
   link,
-}) {
+}: RestoCardProps) {
   return (
     <div className="border p-4 rounded-lg shadow-sm mb-4 bg-white">
       <div className="flex items-center gap-3">
         {logo && (
-          <img
+          <Image
             src={logo}
             alt={name}
+            width={56}
+            height={56}
             className="w-14 h-14 rounded-full object-cover"
           />
         )}
@@ -28,6 +40,7 @@ export default function RestoCard({
         <a
           href={link}
           target="_blank"
+          rel="noreferrer"
           className="mt-3 inline-block text-blue-600 underline"
         >
           Page Facebook
